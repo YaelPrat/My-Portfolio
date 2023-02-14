f891b987074acfab0183923681049961687711ee
YaelPrat <prat.yael@gmail.com>
2023-02-14 13:56:53 +0200

    style & adedd the gefilterFish app <a>

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -5,24 +5,22 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css'],
-  // providers: [NgbCarouselConfig]
 })
 export class CarouselComponent {
   images = [
-    {title: 'First Slide', short: 'First Slide Short', src: "/assets/tree.jpg" ,link: "https://finalproject-baec3.firebaseapp.com/"},
     {title: 'Personal Traning App', short: 'Stack: Angular, firebase', src: "/assets/personalTScreenShot.jpg",link: "https://finalproject-baec3.firebaseapp.com/"},
+    {title: 'Face filter', short: 'Python app, using Dlib 68 points facial landmarks', src: "/assets/gefilterFish.jpg" ,link: "https://github.com/YaelPrat/GefilterFish"}
+    
     // {title: 'Third Slide', short: 'Third Slide Short', src: "/assets/iconCatBig.png"}
   ];
 
+
   constructor(config: NgbCarouselConfig) {
-    // customize default values of carousels used by this component tree
-    config.interval = 4000;
+    config.interval = 6000;
     config.wrap = false;
     config.keyboard = true;
     config.pauseOnHover = true;
-
+    
   }
-  // showNavigationArrows = true;
-  // showNavigationIndicators = true;
-  // interval = 2000;
+ 
 }
